31247d359c4a4bfd19e5cae2f23a4d1632d69ef0
thocle86 <thocle86@gmail.com>
2020-10-19 12:28:42 +0200

    First commit

---
+++ src/Wcs/Hello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Wcs;
+
+class Hello
+{
+    public function talk(): string
+    {
+        return "Hello world !";
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'No version set (parsed as 1.0.0)',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'thocle86/composer',
+  ),
+  'versions' => 
+  array (
+    'ehime/hello-world' => 
+    array (
+      'pretty_version' => '1.0.5',
+      'version' => '1.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b1c8cdd2c11272d8c5deec7816e51fa5374217c1',
+    ),
+    'thocle86/composer' => 
+    array (
+      'pretty_version' => 'No version set (parsed as 1.0.0)',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
